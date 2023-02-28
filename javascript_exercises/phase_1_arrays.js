94e58e3d4259cec11adefc6b9ab3be5543d845af
Kelly Zhou <yinglinzhou0@gmail.com>
2023-02-28 15:45:19 -0500

    phase 1

--- javascript_exercises/phase_1_arrays.js
+++ javascript_exercises/phase_1_arrays.js
@@ -19,3 +19,29 @@ Array.prototype.twoSum = function() {
     }
     return newArr;
 }
+
+// Array.prototype.transpose = function() {
+//     let newLength = this[0].length;
+//     let newArr = Array(newLength).fill([]);
+//     for (i = 0; i < newLength; i++) {
+//         for (j = 0; j < this.length; j++) {
+//             newArr[i].push(this[j][i]);
+//         }
+//     }
+//     return newArr
+// }
+
+
+
+Array.prototype.transpose = function() {
+    let finalArr = [];
+    let i = 0;
+    while (i < this[0].length) {
+        let newArr = [];
+        this.forEach((subArr) =>
+            newArr.push(subArr[i]))
+        finalArr.push(newArr)
+        i++
+    }
+    return finalArr
+}
